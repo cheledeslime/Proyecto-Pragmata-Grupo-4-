bc170780c17000204389fc223adcd2839226c064
TiagoMussini <mussinitiago@gmail.com>
2026-05-25 22:28:49 -0300

    Create Jugador.java

---
+++ Juego/src/Jugador.java
@@ -0,0 +1,37 @@
+public class Jugador {
+
+    private int fila;
+    private int columna;
+    private int puntaje;
+
+    public Jugador() {
+        fila = 0;
+        columna = 0;
+        puntaje = 100;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void mover(int nuevaFila, int nuevaColumna) {
+        fila = nuevaFila;
+        columna = nuevaColumna;
+    }
+
+    public void duplicarPuntaje() {
+        puntaje *= 2;
+    }
+
+    public void sumarPuntos(int puntos) {
+        puntaje += puntos;
+    }
+}
